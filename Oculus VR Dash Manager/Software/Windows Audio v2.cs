9a80d62726f562d610d14df8bbd928c9db1a7155
Eliminater74 <eliminater74@gmail.com>
2023-09-23 18:46:05 -0400

    Windows Audio v2.cs Update

--- Oculus VR Dash Manager/Software/Windows Audio v2.cs
+++ Oculus VR Dash Manager/Software/Windows Audio v2.cs
@@ -8,10 +8,12 @@ namespace OVR_Dash_Manager.Software
 {
     public static class Windows_Audio_v2
     {
+        // Fields
         private static IAudioController controller;
         private static bool _IsSetup = false;
         public static List<IDevice_Ext> Speakers;
 
+        // Method to setup audio controller and initialize speakers list
         public static void Setup()
         {
             if (_IsSetup)
@@ -36,6 +38,7 @@ namespace OVR_Dash_Manager.Software
             _IsSetup = true;
         }
 
+        // Method to set the default playback device
         public static void Set_Default_PlaybackDevice(IDevice Speaker)
         {
             Speaker.SetAsDefault();
@@ -44,6 +47,7 @@ namespace OVR_Dash_Manager.Software
                 Speaker.SetAsDefaultCommunications();
         }
 
+        // Overloaded method to set the default playback device using Speaker ID
         public static void Set_Default_PlaybackDevice(Guid Speaker_ID)
         {
             IDevice Speaker = controller.GetDevice(Speaker_ID);
@@ -51,6 +55,7 @@ namespace OVR_Dash_Manager.Software
                 Set_Default_PlaybackDevice(Speaker);
         }
 
+        // Method to set to normal speaker automatically
         public static void Set_To_Normal_Speaker_Auto(Boolean Force = false)
         {
             if (Properties.Settings.Default.Automatic_Audio_Switching || Force)
@@ -61,6 +66,7 @@ namespace OVR_Dash_Manager.Software
             }
         }
 
+        // Method to set to Quest speaker automatically
         public static void Set_To_Quest_Speaker_Auto(Boolean Force = false)
         {
             if (Properties.Settings.Default.Automatic_Audio_Switching || Force)
@@ -71,10 +77,9 @@ namespace OVR_Dash_Manager.Software
             }
         }
 
+        // Class to extend IDevice with additional properties and notify property changed capability
         public class IDevice_Ext : INotifyPropertyChanged
         {
-            // Add To Class : INotifyPropertyChanged
-
             #region Notify Property Changed Members
 
             public event PropertyChangedEventHandler PropertyChanged;
@@ -90,42 +95,40 @@ namespace OVR_Dash_Manager.Software
 
             #endregion Notify Property Changed Members
 
+            // Constructor to initialize IDevice_Ext object
             public IDevice_Ext(IDevice Speaker)
             {
                 this.Name = Speaker.FullName;
                 this.ID = Speaker.Id;
             }
 
+            // Properties with OnPropertyChanged notification
             private bool _Normal_Speaker;
-
             public bool Normal_Speaker
             {
                 get { return _Normal_Speaker; }
-                set { if (value != _Normal_Speaker) _Normal_Speaker = value; OnPropertyChanged("Normal_Speaker"); }
+                set { if (value != _Normal_Speaker) { _Normal_Speaker = value; OnPropertyChanged("Normal_Speaker"); } }
             }
 
             private bool _Quest_Speaker;
-
             public bool Quest_Speaker
             {
                 get { return _Quest_Speaker; }
-                set { if (value != _Quest_Speaker) _Quest_Speaker = value; OnPropertyChanged("Quest_Speaker"); }
+                set { if (value != _Quest_Speaker) { _Quest_Speaker = value; OnPropertyChanged("Quest_Speaker"); } }
             }
 
             private string _Name;
-
             public string Name
             {
                 get { return _Name; }
-                set { if (value != null || value != _Name) _Name = value; OnPropertyChanged("Name"); }
+                set { if (value != null && value != _Name) { _Name = value; OnPropertyChanged("Name"); } }
             }
 
             private Guid _ID;
-
             public Guid ID
             {
                 get { return _ID; }
-                set { if (value != _ID) _ID = value; OnPropertyChanged("ID"); }
+                set { if (value != _ID) { _ID = value; OnPropertyChanged("ID"); } }
             }
         }
     }

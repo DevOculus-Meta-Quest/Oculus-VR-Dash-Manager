d9db282db8f00b78d232c9fc3daada5eb39b6919
Eliminater74 <eliminater74@gmail.com>
2023-09-23 18:25:02 -0400

    Fix Oculus Link.cs

--- Oculus VR Dash Manager/Software/Oculus Link.cs
+++ Oculus VR Dash Manager/Software/Oculus Link.cs
@@ -1,8 +1,8 @@
-﻿using AdvancedSharpAdbClient;
-using Microsoft.Win32;
-using System;
+﻿using System;
 using System.IO;
 using System.Linq;
+using Microsoft.Win32;
+using AdvancedSharpAdbClient;
 
 namespace OVR_Dash_Manager.Software
 {
@@ -10,30 +10,32 @@ namespace OVR_Dash_Manager.Software
     {
         public static void StartLinkOnDevice()
         {
-            /// ADB Auto Start Created By https://github.com/quagsirus
-            ///
             if (Properties.Settings.Default.QuestPolling)
             {
-                ADB.Start(); // KrisIsBack Addin - This allow when the setting is turned on after launch (only trys if needed to)
+                ADB.Start();  // Ensure ADB is started
 
                 // Allow time for quest to register with ADB server
                 System.Threading.Thread.Sleep(1000);
+
                 var connectedDevices = USB_Devices_Functions.GetUSBDevices();
                 foreach (var device in connectedDevices)
                 {
                     if (string.IsNullOrEmpty(device.FullSerial) || device.Type != "Quest") continue;
+
                     var client = new AdbClient();
                     var adbDevices = client.GetDevices();
+
                     // Ensure adb only interacts with quest device serial nos
                     foreach (var adbDevice in adbDevices.Where(adbDevice => device.FullSerial == adbDevice.Serial))
                     {
                         // Only start quest link if adb has been authorized
-                        if (adbDevice.State == DeviceState.Online) client.StartApp(adbDevice, "com.oculus.xrstreamingclient");
+                        if (adbDevice.State == DeviceState.Online)
+                        {
+                            client.StartApp(adbDevice, "com.oculus.xrstreamingclient");
+                        }
                     }
                 }
             }
-            /// ADB Auto Start Created By https://github.com/quagsirus
-            ///
         }
 
         public static void ResetLink()
@@ -64,22 +66,26 @@ namespace OVR_Dash_Manager.Software
         public static void StartLink()
         {
             if (Service_Manager.GetState("OVRService") != "Running")
+            {
                 Service_Manager.StartService("OVRService");
+            }
         }
 
         public static void SetToOculusRunTime()
         {
             if (Oculus.Oculus_Is_Installed)
             {
-                RegistryKey RunTime = Functions.Registry_Functions.GetRegistryKey(RegistryKey_Type.LocalMachine, @"SOFTWARE\Khronos\OpenXR\1");
-                if (RunTime != null)
+                var runTimeKey = Functions.Registry_Functions.GetRegistryKey(RegistryKey_Type.LocalMachine, @"SOFTWARE\Khronos\OpenXR\1");
+                if (runTimeKey != null)
                 {
-                    String OculusRunTimePath = Path.Combine(Oculus.Oculus_Main_Directory, "Support\\oculus-runtime\\oculus_openxr_64.json");
+                    var oculusRunTimePath = Path.Combine(Oculus.Oculus_Main_Directory, "Support\\oculus-runtime\\oculus_openxr_64.json");
 
-                    if (File.Exists(OculusRunTimePath))
-                        Functions.Registry_Functions.SetKeyValue(RunTime, "ActiveRuntime", OculusRunTimePath);
+                    if (File.Exists(oculusRunTimePath))
+                    {
+                        Functions.Registry_Functions.SetKeyValue(runTimeKey, "ActiveRuntime", oculusRunTimePath);
+                    }
 
-                    Functions.Registry_Functions.CloseKey(RunTime);
+                    Functions.Registry_Functions.CloseKey(runTimeKey);
 
                     Dashes.Dash_Manager.MainForm_CheckRunTime();
                 }

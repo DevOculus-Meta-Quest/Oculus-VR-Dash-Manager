e7bce94d8b229f53678f0bb066914a44f2264f0c
Eliminater74 <eliminater74@gmail.com>
2023-09-23 18:52:53 -0400

    Update Hover Button.cs

--- Oculus VR Dash Manager/Hover Button.cs
+++ Oculus VR Dash Manager/Hover Button.cs
@@ -5,14 +5,28 @@ namespace OVR_Dash_Manager
 {
     internal class Hover_Button
     {
+        // Indicates whether the hover button is enabled or not
         public bool Enabled { get; set; } = false;
+
+        // Indicates whether the hover state is active
         public bool Hovering { get; private set; } = false;
+
+        // Stores the time when the hover state started
         public DateTime Hover_Started { get; private set; } = DateTime.Now;
-        public Int32 Hovered_Seconds_To_Activate { get; set; } = 5;
+
+        // Specifies the duration in seconds to activate the hover state
+        public int Hovered_Seconds_To_Activate { get; set; } = 5;
+
+        // Action to be executed when hover is complete
         public Action Hover_Complete_Action { get; set; }
+
+        // Progress bar to display hover progress
         public ProgressBar Bar { get; set; }
+
+        // Indicates whether to check SteamVR or not
         public bool Check_SteamVR { get; set; }
 
+        // Resets the hover state and progress bar
         public void Reset()
         {
             Hovering = false;
@@ -20,6 +34,7 @@ namespace OVR_Dash_Manager
             Bar.Value = 0;
         }
 
+        // Sets the hover state to active and initializes the progress bar
         public void SetHovering()
         {
             Hovering = true;
@@ -27,6 +42,7 @@ namespace OVR_Dash_Manager
             Bar.Value = 10;
         }
 
+        // Stops the hover state and resets the progress bar
         public void StopHovering()
         {
             Reset();

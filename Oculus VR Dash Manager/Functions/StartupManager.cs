d7c619f8ea8dc20431111ec50eded5bcfd96d411
Eliminater74 <eliminater74@gmail.com>
2023-10-11 12:37:05 -0400

    delete old and add new StartupManger.cs

--- Oculus VR Dash Manager/Functions/StartupManager.cs
+++ Oculus VR Dash Manager/Functions/StartupManager.cs
@@ -1,1 +1,12 @@
-﻿// place setting
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OVR_Dash_Manager.Functions
+{
+    internal class StartupManager
+    {
+    }
+}

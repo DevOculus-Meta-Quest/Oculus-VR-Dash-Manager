262407b5d9c4920a0e62326874266f74fef710ba
Eliminater74 <eliminater74@gmail.com>
2023-10-14 12:36:27 -0400

    FRM_Settings: refactored code.

--- Oculus VR Dash Manager/Forms/Settings/frm_Settings_v2.xaml.cs
+++ Oculus VR Dash Manager/Forms/Settings/frm_Settings_v2.xaml.cs
@@ -4,9 +4,6 @@ using System.Windows.Controls;
 
 namespace OVR_Dash_Manager.Forms.Settings
 {
-    /// <summary>
-    /// Interaction logic for frm_Settings_v2.xaml
-    /// </summary>
     public partial class frm_Settings_v2 : Window
     {
         private bool Audio_DevicesChanged = false;
@@ -19,24 +16,27 @@ namespace OVR_Dash_Manager.Forms.Settings
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            // Set the ItemsSource for the combo boxes and select the appropriate speaker for each
             cbo_NormalSpeaker.ItemsSource = Software.Windows_Audio_v2.Speakers;
             cbo_QuestSpeaker.ItemsSource = Software.Windows_Audio_v2.Speakers;
 
             cbo_NormalSpeaker.SelectedItem = Software.Windows_Audio_v2.Speakers.FirstOrDefault(a => a.Normal_Speaker);
             cbo_QuestSpeaker.SelectedItem = Software.Windows_Audio_v2.Speakers.FirstOrDefault(a => a.Quest_Speaker);
 
+            // Reset flags
             Audio_DevicesChanged = false;
             FireEvents = true;
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            // Check if audio devices have changed and confirm with the user if they want to save the changes
             if (Audio_DevicesChanged)
             {
                 if (MessageBox.Show(this, "Automatic Audio Devices Changed - Are you sure you want to save this ?", "Confirm Automatic Audio Device Change", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
                 {
-                    Software.Windows_Audio_v2.IDevice_Ext NormalSpeaker = Software.Windows_Audio_v2.Speakers.FirstOrDefault(a => a.Normal_Speaker);
-                    Software.Windows_Audio_v2.IDevice_Ext QuestSpeaker = Software.Windows_Audio_v2.Speakers.FirstOrDefault(a => a.Quest_Speaker);
+                    var NormalSpeaker = Software.Windows_Audio_v2.Speakers.FirstOrDefault(a => a.Normal_Speaker);
+                    var QuestSpeaker = Software.Windows_Audio_v2.Speakers.FirstOrDefault(a => a.Quest_Speaker);
 
                     Properties.Settings.Default.Normal_Speaker_GUID = NormalSpeaker != null ? NormalSpeaker.ID : new System.Guid();
                     Properties.Settings.Default.Quest_Speaker_GUID = QuestSpeaker != null ? QuestSpeaker.ID : new System.Guid();
@@ -45,85 +45,58 @@ namespace OVR_Dash_Manager.Forms.Settings
             }
         }
 
-        private void chkbx_Normal_Speaker_Checked(object sender, RoutedEventArgs e)
+        private void CheckSpeaker(Software.Windows_Audio_v2.IDevice_Ext speaker, bool isChecked, bool isNormal, bool isQuest)
         {
-            if (sender is CheckBox Box)
-            {
-                if (Box.DataContext is Software.Windows_Audio_v2.IDevice_Ext Speaker)
-                    CheckSpeaker(Speaker, true, true, false);
-            }
-        }
-
-        private void chkbx_Normal_Speaker_Unchecked(object sender, RoutedEventArgs e)
-        {
-            if (sender is CheckBox Box)
-            {
-                if (Box.DataContext is Software.Windows_Audio_v2.IDevice_Ext Speaker)
-                    CheckSpeaker(Speaker, false, true, false);
-            }
-        }
-
-        private void chkbx_Quest_Speaker_Checked(object sender, RoutedEventArgs e)
-        {
-            if (sender is CheckBox Box)
-            {
-                if (Box.DataContext is Software.Windows_Audio_v2.IDevice_Ext Speaker)
-                    CheckSpeaker(Speaker, true, false, true);
-            }
-        }
-
-        private void chkbx_Quest_Speaker_Unchecked(object sender, RoutedEventArgs e)
-        {
-            if (sender is CheckBox Box)
-            {
-                if (Box.DataContext is Software.Windows_Audio_v2.IDevice_Ext Speaker)
-                    CheckSpeaker(Speaker, false, false, true);
-            }
-        }
-
-        private void CheckSpeaker(Software.Windows_Audio_v2.IDevice_Ext Speaker, bool Checked, bool Normal, bool Quest)
-        {
+            // If FireEvents is false, exit the method early to avoid unnecessary processing
             if (!FireEvents)
                 return;
 
+            // Temporarily disable event firing to prevent potential recursive calls
             FireEvents = false;
 
-            // Force reset to make sure it cant be the same audio device
-            Audio_DevicesChanged = true;
-            Speaker.Quest_Speaker = false;
-            Speaker.Normal_Speaker = false;
+            // Reset speaker statuses
+            speaker.Quest_Speaker = false;
+            speaker.Normal_Speaker = false;
 
-            if (Normal)
+            // Update the speaker statuses based on the parameters passed to the method
+            if (isNormal)
             {
-                Software.Windows_Audio_v2.Speakers.ForEach(a => a.Normal_Speaker = false); // Remove all normals and just set the current
-                Speaker.Normal_Speaker = Checked;
+                // Unset Normal_Speaker for all speakers, then set it for the current speaker if isChecked is true
+                Software.Windows_Audio_v2.Speakers.ForEach(a => a.Normal_Speaker = false);
+                speaker.Normal_Speaker = isChecked;
             }
 
-            if (Quest)
+            if (isQuest)
             {
-                Software.Windows_Audio_v2.Speakers.ForEach(a => a.Quest_Speaker = false); // Remove all quests and just set the current
-                Speaker.Quest_Speaker = Checked;
+                // Unset Quest_Speaker for all speakers, then set it for the current speaker if isChecked is true
+                Software.Windows_Audio_v2.Speakers.ForEach(a => a.Quest_Speaker = false);
+                speaker.Quest_Speaker = isChecked;
             }
 
+            // Re-enable event firing
             FireEvents = true;
         }
 
         private void btn_Set_Default_Normal_Click(object sender, RoutedEventArgs e)
         {
-            Software.Windows_Audio_v2.IDevice_Ext NormalSpeaker = Software.Windows_Audio_v2.Speakers.FirstOrDefault(a => a.Normal_Speaker);
-            if (NormalSpeaker != null)
-                Software.Windows_Audio_v2.Set_Default_PlaybackDevice(NormalSpeaker.ID);
+            // Find the speaker marked as the normal speaker and set it as the default playback device
+            var normalSpeaker = Software.Windows_Audio_v2.Speakers.FirstOrDefault(a => a.Normal_Speaker);
+
+            if (normalSpeaker != null)
+                Software.Windows_Audio_v2.Set_Default_PlaybackDevice(normalSpeaker.ID);
             else
-                MessageBox.Show(this, "Normal Speaker Not Found", "Normal Speaker Not Found", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                MessageBox.Show(this, "Normal Speaker Not Found", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
         }
 
         private void btn_Set_Default_Quest_Click(object sender, RoutedEventArgs e)
         {
-            Software.Windows_Audio_v2.IDevice_Ext QuestSpeaker = Software.Windows_Audio_v2.Speakers.FirstOrDefault(a => a.Quest_Speaker);
-            if (QuestSpeaker != null)
-                Software.Windows_Audio_v2.Set_Default_PlaybackDevice(QuestSpeaker.ID);
+            // Find the speaker marked as the quest speaker and set it as the default playback device
+            var questSpeaker = Software.Windows_Audio_v2.Speakers.FirstOrDefault(a => a.Quest_Speaker);
+
+            if (questSpeaker != null)
+                Software.Windows_Audio_v2.Set_Default_PlaybackDevice(questSpeaker.ID);
             else
-                MessageBox.Show(this, "Quest Speaker Not Set", "Quest Speaker Not Set", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                MessageBox.Show(this, "Quest Speaker Not Set", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
         }
 
         private void cbo_NormalSpeaker_SelectionChanged(object sender, SelectionChangedEventArgs e)

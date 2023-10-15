bde0514a14af2914503255c6bc413bf32cb22cf2
Eliminater74 <eliminater74@gmail.com>
2023-10-15 13:49:45 -0400

    Added New Window Form for Steam App List.

---
+++ Oculus VR Dash Manager/Forms/frm_SteamApps.xaml.cs
@@ -0,0 +1,32 @@
+﻿using OVR_Dash_Manager.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace OVR_Dash_Manager.Forms
+{
+    /// <summary>
+    /// Interaction logic for frm_SteamApps.xaml
+    /// </summary>
+    public partial class frm_SteamApps : Window
+    {
+        public frm_SteamApps()
+        {
+            InitializeComponent();
+            LoadSteamApps();
+        }
+
+        private void LoadSteamApps()
+        {
+            // Retrieve the cached app names using the public method
+            List<string> installedApps = SteamAppChecker.GetInstalledApps();
+
+            // Add the app names to the ListView
+            foreach (var app in installedApps)
+            {
+                listViewSteamApps.Items.Add(app);
+            }
+        }
+    }
+}

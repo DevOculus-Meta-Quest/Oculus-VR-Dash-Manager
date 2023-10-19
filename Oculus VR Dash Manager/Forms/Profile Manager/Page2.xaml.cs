56f1398a596977edd4bf326b8ae8c494f8db7689
Eliminater74 <eliminater74@gmail.com>
2023-10-19 18:49:41 -0400

    Profile Manager Page2

--- Oculus VR Dash Manager/Forms/Profile Manager/Page2.xaml.cs
+++ Oculus VR Dash Manager/Forms/Profile Manager/Page2.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.Windows.Controls;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace OVR_Dash_Manager.Forms.Profile_Manager
 {
@@ -12,32 +12,6 @@ namespace OVR_Dash_Manager.Forms.Profile_Manager
         {
             InitializeComponent();
             this.profileManager = profileManager;
-
-            // Adding event handlers for checkboxes and buttons
-            chkDisableTracking.Checked += ControlStateChanged;
-            chkDisableTracking.Unchecked += ControlStateChanged;
-            chkEnableTracking.Checked += ControlStateChanged;
-            chkEnableTracking.Unchecked += ControlStateChanged;
-            txtTraceFilePath.TextChanged += ControlStateChanged;
-            btnReportState.Click += ControlStateChanged;
-            // ... (add event handlers for other buttons as needed)
-        }
-
-        private void ControlStateChanged(object sender, RoutedEventArgs e)
-        {
-            profileManager.UpdateProfileData("Page2Controls", GetPageControlsState());
-        }
-
-        public void PopulateUI(Dictionary<string, object> profileData)
-        {
-            if (profileData != null && profileData.ContainsKey("Page2Controls"))
-            {
-                var page2Controls = (Dictionary<string, object>)profileData["Page2Controls"];
-                chkDisableTracking.IsChecked = (bool?)page2Controls["chkDisableTracking"];
-                chkEnableTracking.IsChecked = (bool?)page2Controls["chkEnableTracking"];
-                txtTraceFilePath.Text = (string)page2Controls["txtTraceFilePath"];
-                // ... (set other controls as needed)
-            }
         }
 
         public Dictionary<string, object> GetPageControlsState()
@@ -46,9 +20,55 @@ namespace OVR_Dash_Manager.Forms.Profile_Manager
             {
                 { "chkDisableTracking", chkDisableTracking.IsChecked },
                 { "chkEnableTracking", chkEnableTracking.IsChecked },
-                { "txtTraceFilePath", txtTraceFilePath.Text },
-                // ... (add other controls as needed)
+                { "txtTraceFilePath", txtTraceFilePath.Text }
             };
+        }
+
+        public void PopulateUI(Dictionary<string, object> profileData)
+        {
+            if (profileData != null)
+            {
+                profileData.TryGetValue("chkDisableTracking", out object value_chkDisableTracking);
+                chkDisableTracking.IsChecked = value_chkDisableTracking as bool?;
+
+                profileData.TryGetValue("chkEnableTracking", out object value_chkEnableTracking);
+                chkEnableTracking.IsChecked = value_chkEnableTracking as bool?;
+
+                profileData.TryGetValue("txtTraceFilePath", out object value_txtTraceFilePath);
+                txtTraceFilePath.Text = value_txtTraceFilePath as string;
+            }
+        }
+
+        private void Control_Changed(object sender, RoutedEventArgs e)
+        {
+            var controlData = new Dictionary<string, object>
+            {
+                { "chkDisableTracking", chkDisableTracking.IsChecked },
+                { "chkEnableTracking", chkEnableTracking.IsChecked },
+                { "txtTraceFilePath", txtTraceFilePath.Text }
+            };
+
+            profileManager.UpdateProfileData("Page2", controlData);
+        }
+
+        private void Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            string buttonContent = clickedButton.Content.ToString();
+
+            switch (buttonContent)
+            {
+                case "Reset":
+                    // Your code for Reset button
+                    break;
+                case "Select":
+                    // Your code for Select button
+                    break;
+                    // Add cases for other buttons as needed
+            }
+
+            // You might want to update profile data here as well
+            profileManager.UpdateProfileData("Page2", GetPageControlsState());
         }
     }
 }

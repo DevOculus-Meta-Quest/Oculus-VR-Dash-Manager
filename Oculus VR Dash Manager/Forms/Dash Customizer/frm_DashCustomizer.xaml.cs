40b6d5821f48534c10ede5f9f23950c5296558db
Eliminater74 <eliminater74@gmail.com>
2023-10-20 19:08:55 -0400

    Adding Dash Customizer Under Deveopment

---
+++ Oculus VR Dash Manager/Forms/Dash Customizer/frm_DashCustomizer.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Pfim;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using OVR_Dash_Manager.Functions;
+
+namespace OVR_Dash_Manager.Forms.Dash_Customizer
+{
+    /// <summary>
+    /// Interaction logic for frm_DashCustomizer.xaml
+    /// </summary>
+    public partial class frm_DashCustomizer : Window
+    {
+        public frm_DashCustomizer()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertToDDS_Click(object sender, RoutedEventArgs e)
+        {
+            string inputFilePath = txtInputFilePath.Text;
+            string outputFilePath = txtOutputFilePath.Text;
+
+            if (string.IsNullOrWhiteSpace(inputFilePath) || string.IsNullOrWhiteSpace(outputFilePath))
+            {
+                MessageBox.Show("Please select valid input and output files.");
+                return;
+            }
+
+            try
+            {
+                ImageConverter.ConvertPngToDds(inputFilePath, outputFilePath);
+                MessageBox.Show("Conversion to DDS completed successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred during conversion: {ex.Message}");
+            }
+        }
+
+        private void ConvertToPNG_Click(object sender, RoutedEventArgs e)
+        {
+            string inputFilePath = txtInputFilePath.Text;
+            string outputFilePath = txtOutputFilePath.Text;
+
+            if (string.IsNullOrWhiteSpace(inputFilePath) || string.IsNullOrWhiteSpace(outputFilePath))
+            {
+                MessageBox.Show("Please select valid input and output files.");
+                return;
+            }
+
+            try
+            {
+                ImageConverter.ConvertDdsToPng(inputFilePath, outputFilePath);
+                MessageBox.Show("Conversion to PNG completed successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred during conversion: {ex.Message}");
+            }
+        }
+
+        private void BrowseInputFile_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Add This Method
+        }
+
+        private void BrowseOutputFile_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Add This Method
+        }
+    }
+}
---
+++ Oculus VR Dash Manager/Functions/ImageFormatConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using ImageMagick;
+
+
+namespace OVR_Dash_Manager.Functions
+{
+    public static class ImageConverter
+    {
+        public static void ConvertDdsToPng(string inputPath, string outputPath)
+        {
+            using (var image = Pfim.Pfimage.FromFile(inputPath))
+            {
+                PixelFormat format;
+
+                switch (image.Format)
+                {
+                    case Pfim.ImageFormat.Rgba32:
+                        format = PixelFormat.Format32bppArgb;
+                        format = PixelFormat.Format32bppArgb;
+                        break;
+                    default:
+                        throw new NotImplementedException($"The format {image.Format} is not supported");
+                }
+
+                var handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
+                try
+                {
+                    var data = Marshal.UnsafeAddrOfPinnedArrayElement(image.Data, 0);
+                    var bitmap = new Bitmap(image.Width, image.Height, image.Stride, format, data);
+                    bitmap.Save(outputPath, ImageFormat.Png);
+                }
+                finally
+                {
+                    handle.Free();
+                }
+            }
+        }
+
+        public static void ConvertPngToDds(string pngFilePath, string ddsFilePath)
+        {
+            using (MagickImage image = new MagickImage(pngFilePath))
+            {
+                // You can set various options for DDS format, such as compression
+                image.Settings.SetDefine(MagickFormat.Dds, "compression", "dxt5");
+
+                // Save the image as DDS
+                image.Write(ddsFilePath);
+            }
+        }
+    }
+}

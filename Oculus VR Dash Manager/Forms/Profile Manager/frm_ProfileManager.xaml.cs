d472b8ea55b3e535d7a1cda1ea37fa2aba3ac396
Eliminater74 <eliminater74@gmail.com>
2023-10-23 11:32:54 -0400

    Added Some changes For Profile Manager

--- Oculus VR Dash Manager/Forms/Profile Manager/frm_ProfileManager.xaml.cs
+++ Oculus VR Dash Manager/Forms/Profile Manager/frm_ProfileManager.xaml.cs
@@ -1,199 +1,157 @@
-﻿using OVR_Dash_Manager.Functions;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 using YOVR_Dash_Manager.Functions;
 
 namespace OVR_Dash_Manager.Forms.Profile_Manager
 {
+    /// <summary>
+    /// Interaction logic for frm_ProfileManager.xaml
+    /// </summary>
     public partial class frm_ProfileManager : Window
     {
-        private Dictionary<string, object> profileData = new Dictionary<string, object>();
-        private int currentPageNumber = 1;
-        private int totalPageNumber = 8;
+        private Github github;
+        private OculusDebugToolFunctions oculusDebugToolFunctions;
+        private readonly frm_ProfileManager profileManager; // Moved outside of the constructor
 
         public frm_ProfileManager()
         {
             InitializeComponent();
-            LoadProfile("defaultProfile");
-            NavigateToPage(1); // Initial navigation to Page1
+            this.profileManager = profileManager; // Save the reference to the frm_ProfileManager
+            github = new Github();
+            oculusDebugToolFunctions = new OculusDebugToolFunctions();
+            LoadScriptsAsync();
         }
 
-        // This method updates the profileData dictionary when changes are made in the UI.
-        public void UpdateProfileData(string pageKey, Dictionary<string, object> pageControlsData)
-        {
-            // Check if the profileData dictionary already contains data for the specified page
-            if (profileData.ContainsKey(pageKey))
-            {
-                // If it does, update the existing data
-                var existingData = profileData[pageKey] as Dictionary<string, object>;
-                foreach (var item in pageControlsData)
-                {
-                    existingData[item.Key] = item.Value;
-                }
-            }
-            else
-            {
-                // If it doesn't, add a new entry for the page
-                profileData[pageKey] = pageControlsData;
-            }
-        }
-
-        private void SaveProfile(string profileName)
-        {
-            string jsonProfile = JsonFunctions.SerializeClass(profileData);
-
-            string directoryPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            string profilesDirectory = System.IO.Path.Combine(directoryPath, "Profiles");
-            if (!System.IO.Directory.Exists(profilesDirectory))
-            {
-                System.IO.Directory.CreateDirectory(profilesDirectory);
-            }
-
-            string filePath = System.IO.Path.Combine(profilesDirectory, profileName + ".json");
-            System.IO.File.WriteAllText(filePath, jsonProfile);
-        }
-
-        private void LoadProfile(string profileName)
-        {
-            string directoryPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            string filePath = System.IO.Path.Combine(directoryPath, "Profiles", profileName + ".json");
-
-            if (System.IO.File.Exists(filePath))
-            {
-                string jsonProfile = System.IO.File.ReadAllText(filePath);
-                profileData = JsonFunctions.DeserializeClass<Dictionary<string, object>>(jsonProfile);
-
-                Page currentPage = NavigationFrame.Content as Page;
-                if (currentPage != null)
-                {
-                    dynamic currentPageDynamic = Convert.ChangeType(currentPage, currentPage.GetType());
-                    currentPageDynamic.PopulateUI(profileData);
-                }
-            }
-            else
-            {
-                MessageBox.Show("Profile not found.");
-            }
-        }
-
-        private void btnApplyProfile_Click(object sender, RoutedEventArgs e)
+        private async void LoadScriptsAsync()
         {
             try
             {
-                string profileName = "YourProfileName"; // You might get this from a TextBox or another control
-                string profileData = ProfileManagementFunctions.LoadProfile(profileName);
+                string jsonResponse = await github.GetFilesFromDirectoryAsync("DevOculus-Meta-Quest", "OVRDM-Profile-Scripts", "OVRDM-Profile-Scripts");
 
-                if (ProfileManagementFunctions.ApplyProfile(profileData))
+                // Deserialize the JSON response
+                var files = JsonConvert.DeserializeObject<List<GitHubFile>>(jsonResponse);
+
+                scriptsListView.Items.Clear();
+                foreach (var file in files)
                 {
-                    MessageBox.Show("Profile applied successfully!");
+                    scriptsListView.Items.Add(file.name); // Assuming each file object has a 'name' property
+                }
+            }
+            catch (HttpRequestException httpEx)
+            {
+                // Log or display more detailed error information
+                ErrorLogger.LogError(httpEx, $"HTTP Request Failed: {httpEx.Message}");
+                MessageBox.Show($"An HTTP error occurred while loading the scripts: {httpEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.LogError(ex, "An error occurred while loading the scripts.");
+                MessageBox.Show("An error occurred while loading the scripts. Check the error log for details.");
+            }
+        }
+
+        // Define a class to represent the file objects in the JSON response
+        public class GitHubFile
+        {
+            public string name { get; set; }
+            // Add other necessary properties here
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadScriptsAsync();
+        }
+
+        private async void scriptsListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var selectedItem = scriptsListView.SelectedItem as string;
+            if (!string.IsNullOrEmpty(selectedItem))
+            {
+                try
+                {
+                    string scriptUrl = await github.GetFileDownloadUrlAsync("DevOculus-Meta-Quest", "OVRDM-Profile-Scripts", $"OVRDM-Profile-Scripts/{selectedItem}");
+
+                    if (scriptUrl != null)
+                    {
+                        // Download the script content and save it to a temporary file
+                        string tempFilePath = await DownloadAndSaveTempFileAsync(scriptUrl);
+
+                        // Read and display the contents of the temporary file
+                        string fileContents = File.ReadAllText(tempFilePath);
+                        Debug.WriteLine(fileContents); // Output to console
+                        MessageBox.Show(fileContents); // Or display in a message box
+
+                        // Execute the downloaded script file
+                        await oculusDebugToolFunctions.ExecuteCommandWithFileAsync(tempFilePath);
+                        MessageBox.Show($"Executed the script: {selectedItem}");
+
+                        // Optionally, delete the temporary file after execution
+                        File.Delete(tempFilePath);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error: Script not found on GitHub.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ErrorLogger.LogError(ex, "An error occurred while executing the script.");
+                    MessageBox.Show("An error occurred while executing the script. Check the error log for details.");
+                }
+            }
+        }
+
+        private async Task<string> DownloadAndSaveTempFileAsync(string downloadUrl)
+        {
+            using HttpClient httpClient = new HttpClient();
+
+            // Fetch the content from the URL
+            HttpResponseMessage response = await httpClient.GetAsync(downloadUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Read the content as a string
+                string fileContent = await response.Content.ReadAsStringAsync();
+
+                if (!string.IsNullOrEmpty(fileContent))
+                {
+                    // Creating a temporary file with a specific extension if needed
+                    string tempFilePath = Path.GetTempFileName();
+
+                    // Writing the content fetched from the URL into the temporary file with UTF-8 encoding
+                    await File.WriteAllTextAsync(tempFilePath, fileContent, Encoding.UTF8);
+
+                    return tempFilePath;
                 }
                 else
                 {
-                    MessageBox.Show("Failed to apply profile.");
+                    ErrorLogger.LogError(null, "The file content fetched from GitHub is empty.");
+                    throw new Exception("The file content fetched from GitHub is empty.");
                 }
             }
-            catch (Exception ex)
+            else
             {
-                ErrorLogger.LogError(ex, "Failed to apply profile.");
+                ErrorLogger.LogError(null, $"Failed to fetch the file from GitHub. Status Code: {response.StatusCode}");
+                throw new Exception($"Failed to fetch the file from GitHub. Status Code: {response.StatusCode}");
             }
         }
 
-        // This is a method that you might call when a Save button is clicked.
-        private void btnSaveProfile_Click(object sender, RoutedEventArgs e)
+        private async void btnExecuteCommand_Click(object sender, EventArgs e)
         {
-            try
-            {
-                string profileName = "YourProfileName"; // You might get this from a TextBox or another control
-                SaveProfile(profileName);
-                MessageBox.Show("Profile saved successfully!");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Failed to save profile.");
-                ErrorLogger.LogError(ex, "Failed to save profile.");
-            }
-        }
-
-        private void btnFirst_Click(object sender, RoutedEventArgs e)
-        {
-            NavigateToPage(1);
-        }
-
-        private void btnPrev_Click(object sender, RoutedEventArgs e)
-        {
-            if (currentPageNumber > 1)
-            {
-                NavigateToPage(currentPageNumber - 1);
-            }
-        }
-
-        private void btnPage_Click(object sender, RoutedEventArgs e)
-        {
-            Button btn = sender as Button;
-            int pageNumber = int.Parse(btn.Content.ToString());
-            NavigateToPage(pageNumber);
-        }
-
-        private void btnNext_Click(object sender, RoutedEventArgs e)
-        {
-            if (currentPageNumber < totalPageNumber)
-            {
-                NavigateToPage(currentPageNumber + 1);
-            }
-        }
-
-        private void btnLast_Click(object sender, RoutedEventArgs e)
-        {
-            NavigateToPage(totalPageNumber);
-        }
-
-        private void NavigateToPage(int pageNumber)
-        {
-            Debug.WriteLine($"Navigating to Page {pageNumber}"); // Debug output
-            // Save the current page's data before navigating away
-            if (NavigationFrame.Content is Page currentPage)
-            {
-                dynamic currentPageDynamic = Convert.ChangeType(currentPage, currentPage.GetType());
-                var currentPageData = currentPageDynamic.GetPageControlsState();
-                foreach (var item in currentPageData)
-                {
-                    profileData[item.Key] = item.Value;
-                }
-            }
-
-            currentPageNumber = pageNumber;
-
-            switch (pageNumber)
-            {
-                case 1:
-                    Debug.WriteLine("Initializing Page 1"); // Debug output
-                    var page1 = new Page1(this);
-                    page1.PopulateUI(profileData); // Load the saved profile data
-                    NavigationFrame.Navigate(page1);
-                    break;
-
-                case 2:
-                    Debug.WriteLine("Initializing Page 2"); // Debug output
-                    var page2 = new Page2(this);
-                    page2.PopulateUI(profileData); // Load the saved profile data
-                    NavigationFrame.Navigate(page2);
-                    break;
-
-                case 3:
-                    Debug.WriteLine("Initializing Page 3"); // Debug output
-                    var page3 = new Page3(this);
-                    page3.PopulateUI(profileData); // Load the saved profile data
-                    NavigationFrame.Navigate(page3);
-                    break;
-                // ... (similar code for other pages like Page3, Page4, etc.)
-
-                default:
-                    MessageBox.Show("Page not found.");
-                    break;
-            }
         }
     }
 }
--- Oculus VR Dash Manager/Forms/frm_OtherTools.xaml.cs
+++ Oculus VR Dash Manager/Forms/frm_OtherTools.xaml.cs
@@ -6,73 +6,73 @@ using System.Windows;
 
 namespace OVR_Dash_Manager.Forms
 {
-    /// <summary>
+/// <summary>
     /// Interaction logic for frm_OtherTools.xaml
-    /// </summary>
-    public partial class frm_OtherTools : Window
-    {
-        public frm_OtherTools()
-        {
-            InitializeComponent();
-        }
+/// </summary>
+public partial class frm_OtherTools : Window
+{
+public frm_OtherTools()
+{
+InitializeComponent();
+}
 
-        private void btn_InstallAPK_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog
-            {
-                Filter = "APK files (*.apk)|*.apk",
-                Title = "Select an APK file"
-            };
-            if (openFileDialog.ShowDialog() == true)
-            {
-                ADB.InstallAPK(openFileDialog.FileName);
-            }
-        }
+private void btn_InstallAPK_Click(object sender, RoutedEventArgs e)
+{
+OpenFileDialog openFileDialog = new OpenFileDialog
+{
+Filter = "APK files (*.apk)|*.apk",
+Title = "Select an APK file"
+};
+if (openFileDialog.ShowDialog() == true)
+{
+ADB.InstallAPK(openFileDialog.FileName);
+}
+}
 
-        private void btn_SteamAppsShow_Click(object sender, RoutedEventArgs e)
-        {
-            // Create an instance of frm_SteamApps window
-            var steamAppsWindow = new frm_SteamApps();
+private void btn_SteamAppsShow_Click(object sender, RoutedEventArgs e)
+{
+// Create an instance of frm_SteamApps window
+var steamAppsWindow = new frm_SteamApps();
 
-            // Set the frm_OtherTools window as the owner of steamAppsWindow
-            steamAppsWindow.Owner = this;
+// Set the frm_OtherTools window as the owner of steamAppsWindow
+steamAppsWindow.Owner = this;
 
-            // Show the window
-            steamAppsWindow.Show();
-        }
+// Show the window
+steamAppsWindow.Show();
+}
 
-        private void btn_OculusAppsShow_Click(object sender, RoutedEventArgs e)
-        {
-            // Create an instance of frm_SteamApps window
-            var OculusAppsWindow = new frm_OculusApps();
+private void btn_OculusAppsShow_Click(object sender, RoutedEventArgs e)
+{
+// Create an instance of frm_SteamApps window
+var OculusAppsWindow = new frm_OculusApps();
 
-            // Set the frm_OtherTools window as the owner of steamAppsWindow
-            OculusAppsWindow.Owner = this;
+// Set the frm_OtherTools window as the owner of steamAppsWindow
+OculusAppsWindow.Owner = this;
 
-            // Show the window
-            OculusAppsWindow.Show();
-        }
+// Show the window
+OculusAppsWindow.Show();
+}
 
-        private void btn_ADBFileManager_Click(object sender, RoutedEventArgs e)
-        {
-            ADBFileManagerWindow adbFileManagerWindow = new ADBFileManagerWindow();
-            adbFileManagerWindow.ShowDialog();
-        }
+private void btn_ADBFileManager_Click(object sender, RoutedEventArgs e)
+{
+ADBFileManagerWindow adbFileManagerWindow = new ADBFileManagerWindow();
+adbFileManagerWindow.ShowDialog();
+}
 
-        private void btn_ProfileManager_Click(object sender, RoutedEventArgs e)
-        {
-            // Create an instance of the frm_ProfileManager window and show it
-            frm_MainProfileManager profileManager = new frm_MainProfileManager();
-            profileManager.Owner = this; // 'this' refers to the main window
-            profileManager.ShowDialog();
-        }
+private void btn_PNG2DDS_Click(object sender, RoutedEventArgs e)
+{
+frm_DashCustomizer dashCustomizer = new frm_DashCustomizer();
+dashCustomizer.WindowStartupLocation = WindowStartupLocation.CenterScreen; // Center the window
+dashCustomizer.Topmost = true; // Make the window appear on top of other windows
+dashCustomizer.ShowDialog(); // Open the window as a modal dialog box
+}
 
-        private void btn_PNG2DDS_Click(object sender, RoutedEventArgs e)
-        {
-            frm_DashCustomizer dashCustomizer = new frm_DashCustomizer();
-            dashCustomizer.WindowStartupLocation = WindowStartupLocation.CenterScreen; // Center the window
-            dashCustomizer.Topmost = true; // Make the window appear on top of other windows
-            dashCustomizer.ShowDialog(); // Open the window as a modal dialog box
-        }
-    }
+private void btn_ProfileManager1_Click(object sender, RoutedEventArgs e)
+{
+// Create an instance of the frm_ProfileManager window and show it
+frm_ProfileManager profileManager = new frm_ProfileManager();
+profileManager.Owner = this; // 'this' refers to the main window
+profileManager.ShowDialog();
+}
+}
 }

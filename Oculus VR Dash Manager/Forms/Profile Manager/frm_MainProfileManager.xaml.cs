3f351b433e2cb0e812234315395ab10a1dfacfbb
Eliminater74 <eliminater74@gmail.com>
2023-10-22 21:30:59 -0400

    ProfileManagerGH Added New Edition

---
+++ Oculus VR Dash Manager/Forms/Profile Manager/frm_MainProfileManager.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace OVR_Dash_Manager.Forms.Profile_Manager
+{
+    /// <summary>
+    /// Interaction logic for frm_MainProfileManager.xaml
+    /// </summary>
+    public partial class frm_MainProfileManager : Window
+    {
+        public frm_MainProfileManager()
+        {
+            InitializeComponent();
+            Debug.WriteLine("frm_MainProfileManager initialized."); // Debug log
+        }
+
+        private void btn_ProfileManagerGH_Click(object sender, RoutedEventArgs e)
+        {
+            // Create an instance of the frm_ProfileManager window and show it
+            frm_ProfileManagerGH profileManager = new frm_ProfileManagerGH();
+            profileManager.Owner = this; // 'this' refers to the main window
+            profileManager.ShowDialog();
+        }
+
+        private void btn_ProfileManager_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}

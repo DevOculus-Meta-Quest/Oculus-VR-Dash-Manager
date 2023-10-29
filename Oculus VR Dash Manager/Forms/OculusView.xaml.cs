ccd46688dbf839b73a0e394af651c1307eb719d7
Eliminater74 <eliminater74@gmail.com>
2023-10-29 14:49:53 -0400

    Code got messed up, this cleaned  it

--- Oculus VR Dash Manager/Forms/OculusView.xaml.cs
+++ Oculus VR Dash Manager/Forms/OculusView.xaml.cs
@@ -4,39 +4,39 @@ using System.Windows;
 
 namespace OVR_Dash_Manager.Forms
 {
-    public partial class OculusView : Window
-    {
-        private OculusControllerHandler _oculusHandler;
+public partial class OculusView : Window
+{
+private OculusControllerHandler _oculusHandler;
 
-        public OculusView()
-        {
-            InitializeComponent();
-            _oculusHandler = new OculusControllerHandler(UpdateListView);
+public OculusView()
+{
+InitializeComponent();
+_oculusHandler = new OculusControllerHandler(UpdateListView);
 
-            // Start monitoring when the window is loaded
-            Loaded += (s, e) => StartMonitoringController();
-        }
-
-        private void UpdateListView(string message)
-        {
-            oculusInfoListView.Items.Add(new { Information = message });
-        }
-
-        public void StartMonitoringController()
-        {
-            _oculusHandler.MonitorController();
-        }
-
-        public void StopMonitoringController()
-        {
-            _oculusHandler.Cleanup();
-        }
-
-        // Override the OnClosed method to ensure that the Oculus session is cleaned up properly
-        protected override void OnClosed(EventArgs e)
-        {
-            StopMonitoringController();
-            base.OnClosed(e);
-        }
+// Start monitoring when the window is loaded
+Loaded += (s, e) => StartMonitoringController();
     }
-}
+
+    private void UpdateListView(string message)
+    {
+    oculusInfoListView.Items.Add(new { Information = message });
+    }
+
+    public void StartMonitoringController()
+    {
+    _oculusHandler.MonitorController();
+    }
+
+    public void StopMonitoringController()
+    {
+    _oculusHandler.Cleanup();
+    }
+
+    // Override the OnClosed method to ensure that the Oculus session is cleaned up properly
+    protected override void OnClosed(EventArgs e)
+    {
+    StopMonitoringController();
+    base.OnClosed(e);
+    }
+    }
+    }

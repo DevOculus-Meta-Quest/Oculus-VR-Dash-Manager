d7c9da500c6002bb917e09b12e8504a7723ebf84
Eliminater74 <eliminater74@gmail.com>
2023-11-21 13:53:52 -0500

    Add Splash Screen

--- Oculus VR Dash Manager/Properties/Settings.Designer.cs
+++ Oculus VR Dash Manager/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace OVR_Dash_Manager.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -248,6 +248,18 @@ namespace OVR_Dash_Manager.Properties {
             }
             set {
                 this["MinToTray"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool ShowSplashScreen {
+            get {
+                return ((bool)(this["ShowSplashScreen"]));
+            }
+            set {
+                this["ShowSplashScreen"] = value;
             }
         }
     }
---
+++ Oculus VR Dash Manager/Splash.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OVR_Dash_Manager
+{
+    /// <summary>
+    /// Interaction logic for Splash.xaml
+    /// </summary>
+    public partial class Splash : Window
+    {
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        private void chkDontShowAgain_Checked(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.ShowSplashScreen = !chkDontShowAgain.IsChecked.Value;
+            Properties.Settings.Default.Save();
+        }
+    }
+}

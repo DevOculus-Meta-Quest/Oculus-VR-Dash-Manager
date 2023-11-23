d69fac9be101f642f284e95de8d06fa07ab0444d
Eliminater74 <eliminater74@gmail.com>
2023-11-23 13:28:43 -0500

    Add WelcomeWindow:  Run Only Once

---
+++ Oculus VR Dash Manager/Forms/WelcomeWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace OVR_Dash_Manager.Forms
+{
+    /// <summary>
+    /// Interaction logic for WelcomeWindow.xaml
+    /// </summary>
+    public partial class WelcomeWindow : Window
+    {
+        public WelcomeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = e.Uri.AbsoluteUri,
+                UseShellExecute = true
+            });
+            e.Handled = true;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
